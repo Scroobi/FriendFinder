555bd7692773b9954ade6a2ac700e4734de7c718
bryce <blrippey@gmail.com>
2019-05-23 14:03:26 -0700

    script

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,11 @@
 var path = require("path")
 
 module.exports = function (app) {
-
   app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+    res.sendFile(path.join(__dirname + "/../public/survey.html"));
   });
 
-  app.get('*', function (req, res) {
-    res.sendfile(path.join(__dirname, "/app/public/index.html"));
+  app.use(function (req, res) {
+    res.sendFile(path.join(__dirname + "/../public/home.html"));
   });
-
-}
+};
--- server.js
+++ server.js
@@ -1,25 +1,18 @@
-// Dependencies
-// =============================================================
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
 
-// Sets up the Express App
-// =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use(express.static(path.join(__dirname, './app/public')));
+app.use(express.static("app/public"));
 
-// require in routes
-//require(path.join(__dirname, './app/routing/apiRoutes'))(app);
-require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-});
+app.listen(port, () => console.log("Listening on port %s", port));
